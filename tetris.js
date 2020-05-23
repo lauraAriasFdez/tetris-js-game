00cf833e52de262323bf752cb07dd492fe4c322e
lauraAriasFdez <52958827+lauraAriasFdez@users.noreply.github.com>
2020-05-23 13:16:51 +0200

    update rotation of figures

--- tetris.js
+++ tetris.js
@@ -25,8 +25,8 @@ class Person{
         this.position = [x,y];
     }
 
-    setMatrix(matrix) {
-        this.matrix = matrix;
+    setBlock(matrix) {
+        this.block = matrix;
     }
 
 
@@ -35,8 +35,22 @@ class Person{
     }
 }
 
-
+/* GAAAAAMEEEE*/
 {
+
+    /*create canvas matrix;
+    function createMatrix(width, height){
+        let matrixTetris = [];
+        for (var i =0; i<width;i++){
+            let row = [];
+            for (var j=0; j <height;j++){
+                row.push = 0;
+            }
+            matrixTetris.push(row);
+        }
+        return matrixTetris;
+    }*/
+
     /*initialize variables and blocks matrix*/
     let blockElements = [
         new Block ([[0,0,0,0],[0,0,1,0],[1,1,1,0],[0,0,0,0]],"orange"),
@@ -47,12 +61,19 @@ class Person{
         new Block ([[0,0,0,0],[0,0,0,0],[1,1,1,1],[0,0,0,0]], "cyan") ,
         new Block ([[0,0,0,0],[1,0,0,0],[1,1,1,0],[0,0,0,0]],"pink")
     ];
+
+    /*gridObjects are the objects who have been position already*/
     let gridObjects = [];
+    //let gridMatrix = createMatrix(Number.parseInt(600/15),Number.parseInt(700/6));
+
+    /*main variables*/
     let tetris = document.getElementById("game-grid");
     let brush = tetris.getContext("2d");
+    brush.scale(15,6);
     let player = new Person (5,5);
+    player.setBlock( blockElements[Math.floor(Math.random() * blockElements.length)]);
 
-
+    /*next Block player will use*/
     let nextBlock = blockElements[Math.floor(Math.random() * blockElements.length)];
     let nextBlockDisplay = document.getElementById("piece");
     let nextBlockBrush = nextBlockDisplay.getContext("2d");
@@ -60,9 +81,7 @@ class Person{
     drawNextBlock(nextBlockBrush,nextBlock);
 
     
-    player.setMatrix( blockElements[0]);
-    brush.scale(15,6);
-
+    
     
     //Event listner
    document.addEventListener("keydown",function(event){
@@ -70,6 +89,7 @@ class Person{
        //update pastPosition
        switch(event){
            case "ArrowRight":
+               console.log(tetris.width);
                moveRight();
                break;
             case "ArrowLeft":
@@ -82,10 +102,10 @@ class Person{
                 if (checkBlock())
                     moveDown();
                 else{
-                    
-                    gridObjects. push ([player.matrix,player.position]);
+                    console.log(player.block,player.position);
+                    gridObjects. push ([player.block,player.position]);
                     //generate a random block
-                    player.setMatrix(nextBlock);
+                    player.setBlock(nextBlock);
                     nextBlock = blockElements[Math.floor(Math.random() * blockElements.length)];
                     drawNextBlock(nextBlockBrush,nextBlock);
                 }
@@ -104,22 +124,39 @@ class Person{
     }
 
     function rotate(){
-        console.log("rotate");
-        let grid =  player.matrix.getGrid();        
+        let grid =  player.block.getGrid();        
         let newgrid = [];
-        // for j representing the col 
-        for (var j=0; j<grid.length; j++){
-            let i =grid.length-1;
-            let row = [];
-            while (i>=0){
-                row.push(grid[i][j]);
-                i--;
-            }
-            console.log(row);
-            newgrid.push(row);
+        if (player.block.color=== "cyan"){
+            // matrix N x N rotation 
+            for (var j=0; j<grid.length; j++){
+                let i =grid.length-1;
+                let row = [];
+                while (i>=0){
+                    row.push(grid[i][j]);
+                    i--;
+                }
+                newgrid.push(row);
+            } 
+             //mutate block grid
+             player.block.matrix = newgrid;
+
+        } else if (player.block.color!=="yellow"){
+            // matrix N x N rotation but ignoring last col and top row which is always [0,0,0,0]
+            newgrid.push([0,0,0,0]);
+            for (var j=0; j<grid.length-1; j++){
+                let i =grid.length-1;
+                let row = [];
+                while (i>=0){
+                    row.push(grid[i][j]);
+                    i--;
+                }
+                row.push(0);
+                newgrid.push(row);
+            } 
+            //mutate block grid
+            player.block.matrix = newgrid;
         }
-        //mutate block grid
-        player.matrix.matrix = newgrid;
+        
     }
 
    function checkBlock(){
@@ -133,7 +170,7 @@ class Person{
     function draw (){
         brush.fillStyle = "black";
         brush.fillRect(0,0,tetris.width , tetris.height);
-        player.matrix.drawBlock(brush,player.position);
+        player.block.drawBlock(brush,player.position);
 
         for (var i =0; i<gridObjects.length;i++){
             gridObjects[i][0].drawBlock(brush,gridObjects[i][1]);
@@ -161,6 +198,8 @@ class Person{
                 break;
         }
        
+
+       
         
     }
 
@@ -168,11 +207,11 @@ class Person{
 
         //test, main methods
     /*
-    player.matrix.drawBlock(brush,player.position);
-    console.log(player.matrix, player.position);
+    player.block.drawBlock(brush,player.position);
+    console.log(player.block, player.position);
     player.setPosition(10,10);
-    console.log(player.matrix, player.position);
-    player.matrix.drawBlock(brush,player.position);
+    console.log(player.block, player.position);
+    player.block.drawBlock(brush,player.position);
 */
 
 }
